31b1f81ee8a857bf5c5324c0e0201d70c9ec8823
Guilherme Jabur <guilhermejabur@outlook.com>
2018-12-04 01:48:45 -0200

    add styleguide

--- app/src/config/theme.tsx
+++ app/src/config/theme.tsx
@@ -1,10 +1,14 @@
 export default {
   colors: {
     primaryText: '#ffffff',
-    secondaryText: 'grey',
+    secondaryText: '#7F00FF',
+    disabled: 'grey',
     primaryBackground: '#7F00FF',
   },
   fontSizes: {
-
+    small: 13,
+    normal: 16,
+    big: 25,
+    huge: 53,
   }
 }
