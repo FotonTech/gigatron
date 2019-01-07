f04b6ae3f8fd9a955e750371e3a6ce7576f763f7
Guilherme Jabur <guilhermejabur@outlook.com>
2019-01-07 17:05:20 -0200

    use now-env

--- packages/server/dist/config.js
+++ packages/server/dist/config.js
@@ -3,39 +3,18 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-/*import path from 'path';
-import dotenvSafe from 'dotenv-safe';
-
-const root = path.join.bind(this, __dirname, '../');
-
-dotenvSafe.load({
-  path: root('.env'),
-});
+require('now-env');
 
 // Database Settings
-const dBdevelopment = process.env.MONGO_URL || 'mongodb://localhost/fotonChat';
-const dBproduction = process.env.MONGO_URL || 'mongodb://localhost/fotonChat';
+var dBdevelopment = process.env.MONGO_URL || 'mongodb://localhost/fotonChat';
+var dBproduction = process.env.MONGO_URL || 'mongodb://localhost/fotonChat';
 
 // Test Database Settings
 // const test = 'mongodb://localhost/awesome-test';
 
 // Export DB Settings
-export const databaseConfig = process.env.NODE_ENV === 'production' ? dBproduction : dBdevelopment;
+var databaseConfig = exports.databaseConfig = process.env.NODE_ENV === 'production' ? dBproduction : dBdevelopment;
 
 // Export GraphQL Server settings
-export const graphqlPort = process.env.GRAPHQL_PORT || 5000;
-export const jwtSecret = process.env.JWT_KEY || 'secret_key';
-*/
-
-// Database Settings
-var dBdevelopment = 'mongodb://localhost/fotonChat';
-
-// Test Database Settings
-// const test = 'mongodb://localhost/awesome-test';
-
-// Export DB Settings
-var databaseConfig = exports.databaseConfig = dBdevelopment;
-
-// Export GraphQL Server settings
-var graphqlPort = exports.graphqlPort = 5000;
-var jwtSecret = exports.jwtSecret = 'secret_key';
+var graphqlPort = exports.graphqlPort = process.env.GRAPHQL_PORT || 5000;
+var jwtSecret = exports.jwtSecret = process.env.JWT_KEY || 'secret_key';
--- packages/server/src/config.js
+++ packages/server/src/config.js
@@ -1,11 +1,4 @@
-/*import path from 'path';
-import dotenvSafe from 'dotenv-safe';
-
-const root = path.join.bind(this, __dirname, '../');
-
-dotenvSafe.load({
-  path: root('.env'),
-});
+require('now-env');
 
 // Database Settings
 const dBdevelopment = process.env.MONGO_URL || 'mongodb://localhost/fotonChat';
@@ -20,17 +13,3 @@ export const databaseConfig = process.env.NODE_ENV === 'production' ? dBproducti
 // Export GraphQL Server settings
 export const graphqlPort = process.env.GRAPHQL_PORT || 5000;
 export const jwtSecret = process.env.JWT_KEY || 'secret_key';
-*/
-
-// Database Settings
-const dBdevelopment = 'mongodb://localhost/fotonChat';
-
-// Test Database Settings
-// const test = 'mongodb://localhost/awesome-test';
-
-// Export DB Settings
-export const databaseConfig = dBdevelopment;
-
-// Export GraphQL Server settings
-export const graphqlPort = 5000;
-export const jwtSecret = 'secret_key';
