6be02ee59dc6f75e415628c8a61fa5487bfdfbb4
Guilherme Jabur <guilhermejabur@outlook.com>
2019-01-07 00:55:57 -0200

    fix assets

--- app/src/assets/index.tsx
+++ app/src/assets/index.tsx
@@ -1,14 +1,5 @@
 export const images = {
-  deadDog: require('./dogDead.png'),
-  happyDog: require('./dogHappy.png'),
-  sleepyDog: require('./dogSleeping.png'),
-  pug: require('./pug.png'),
   backArrow: require('./backArrow.png'),
   correctSign: require('./correctSign.png'),
-  Banho: require('./Banho.png'),
-  Bisturi: require('./Bisturi.png'),
-  Estetoscopio: require('./Estetoscopio.png'),
-  Minhoca: require('./Minhoca.png'),
-  Remedio: require('./Remedio.png'),
-  Seringa: require('./Seringa.png'),
+  hang: require('./hang.jpg'),
 };
