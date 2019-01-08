ecc62c527ebd973f2e107bbb6cbc16a21b80f28c
Guilherme Jabur <guilhermejabur@outlook.com>
2019-01-08 00:30:59 -0200

    fix dependencie/babel

---
+++ packages/babel-preset-gigatron/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "presets": [
+      "@babel/env",
+      "@babel/preset-typescript"
+  ],
+  "plugins": [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-object-rest-spread"
+  ]
+}
