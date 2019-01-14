7858fbfcffe0ac27d693f3c7a22bd83fac67ee4c
Gustavo Maes <gustavomaescd@gmail.com>
2019-01-14 19:34:29 -0200

    normalize port

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,9 @@ import { ApolloServer } from 'apollo-server';
 import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
 import rootQuery from './modules/rootQuery';
 import * as mongoose from "mongoose";
+import * as dotenv from "dotenv"
+
+dotenv.config()
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
@@ -18,7 +21,22 @@ mongoose.connection.once("open", () => {
 })
 
 const server = new ApolloServer({ schema });
+const port = normalizePort(process.env.PORT || '4000')
 
-server.listen().then(({ url }) => {
+server.listen(port).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
+
+function normalizePort(val) {
+  const port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    return port
+  }
+
+  if (port >= 0) {
+    return port
+  }
+
+  return false
+}
