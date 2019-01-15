0c5a7e40c6cdd62a619e5902bad3e846ea420d80
Gustavo Maes <gustavomaescd@gmail.com>
2019-01-15 14:28:24 -0200

    theme file

---
+++ packages/app/src/theme.tsx
@@ -0,0 +1,12 @@
+export default {
+  color: {
+    primary: '#C4AF95',
+    secondary: '#222222',
+    primaryBackground: '#FBFBFB',
+  },
+  fontSize: {
+    large: 18,
+    normal: 14,
+    small: 12,
+  }
+};
