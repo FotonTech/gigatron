b301612ce0fae1677ea7ce2013cfac6c2e201ef7
Gustavo Maes <gustavomaescd@gmail.com>
2019-01-15 15:15:41 -0200

    common input

---
+++ packages/app/src/components/Input.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import styled from 'styled-components/native'
+
+const Wrapper = styled.View`
+  align-self: center;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 28;
+`
+
+const StyledInput = styled.TextInput`
+  height: 40;
+  width: 80%;
+  font-size: ${(p: any) => p.theme.fontSize.large};
+  border-width: 0;
+  border-bottom-width: 2;
+`
+
+
+interface Props {
+  value: string
+  placeholder: string
+  onChange: (val: any) => void
+  keyboardType?: string
+}
+
+
+const Input = ({
+  value,
+  placeholder,
+  onChange,
+  ...rest
+}: Props) => {
+  return (
+    <Wrapper>
+      <StyledInput
+        onChangeText={(val: string) => onChange(val)}
+        value={value}
+        autoCapitalize="none"
+        {...rest}
+      />
+    </Wrapper>
+  )
+}
+
+export default Input
