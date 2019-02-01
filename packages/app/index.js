89fc34c18acdfc0f130b8190785d0cd000cc4127
Guilherme Jabur <guilhermejabur@outlook.com>
2019-02-01 16:58:18 -0200

    nohoist app package

--- packages/app/index.js
+++ packages/app/index.js
@@ -1,7 +1,10 @@
-/** @format */
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- packages/app/src/navigation/RouteName.tsx
+++
@@ -1,9 +0,0 @@
-export const ROUTENAMES = {
-  //Apps
-  NON_LOGGED_APP: 'NonLoggedAppRouter',
-  LOGGED_APP: 'LoggedApp',
-  //Screend
-  SIGNIN: 'Signin',
-  SIGNUP: 'SignUp',
-  HOME: 'Home',
-};
