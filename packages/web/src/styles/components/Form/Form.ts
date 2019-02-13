0b987c8260f6c1551a278c96360d40d0f7b04901
leonardomso <leonardomso11@gmail.com>
2019-02-13 19:05:05 -0200

    Better Styled Components with TypeScript props

---
+++ packages/web/src/styles/components/Form/Form.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const Form = styled.div``;
+
+export default Form;
---
+++ packages/web/src/styles/components/Input/Input.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const Input = styled.div``;
+
+export default Input;
