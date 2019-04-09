379405cfea55b17784a9746336c1a1d5185d08eb
Raphael Thomazella <thomazella9@gmail.com>
2019-04-09 12:04:25 -0300

    link open-settings

--- packages/app/android/app/src/main/java/com/foton/app/MainApplication.java
+++ packages/app/android/app/src/main/java/com/foton/app/MainApplication.java
@@ -3,6 +3,7 @@ package com.foton.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.opensettings.OpenSettingsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -21,7 +22,8 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(new MainReactPackage());
+      return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new OpenSettingsPackage());
     }
 
     @Override
