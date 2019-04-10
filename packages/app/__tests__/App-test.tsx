3bdbc9b767780c11435ef7e34711840f73c2328c
Raphael Thomazella <thomazella9@gmail.com>
2019-04-10 01:40:51 -0300

    fix babel name, rm jest from app

--- packages/app/__tests__/App-test.tsx
+++
@@ -1,8 +0,0 @@
-import 'react-native'
-import React from 'react'
-import App from '../App'
-import renderer from 'react-test-renderer'
-
-it('renders correctly', () => {
-  renderer.create(<App />)
-})
--- packages/app/babel.config.js
+++ packages/app/babel.config.js
@@ -1,3 +1,6 @@
+const rootbabel = require('../../babel.config')
+
 module.exports = {
+  ...rootbabel(),
   presets: ['module:metro-react-native-babel-preset'],
 }
