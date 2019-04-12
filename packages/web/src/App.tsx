0ff0b0a42a6405a826d15a497f58ef5661fc191e
Raphael Thomazella <thomazella9@gmail.com>
2019-04-11 23:35:17 -0300

    init new web app (#28)
    * init new web app
    
    * downgrade eslint to make cra happy
    
    * rm yarn.lock in packages/web

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,73 +1,28 @@
-import React, { Fragment } from "react";
-import { ThemeProvider } from "styled-components";
-import { ApolloProvider } from "react-apollo";
-import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
-import styled, { createGlobalStyle } from "styled-components";
-import { BrowserRouter, Switch } from "react-router-dom";
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-import Login from "./components/Login/Login";
-import UsersTest from "./components/Users/Users";
-import Users from "./screens/Users";
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
 
-import PrivateRoute from "./routes/Private";
-import PublicRoute from "./routes/Public";
-
-import { theme } from "./styles/Theme/Theme";
-import client from "./graphql/client";
-import reset from "./styles/constants/reset";
-
-const GlobalStyle = createGlobalStyle`${reset}`;
-
-const Wrapper = styled.div`
-    display: flex;
-    flex: 1;
-    min-height: 100vh;
-    flex-direction: column;
-    background: linear-gradient(90deg, #fc466b 0%, #3f5efb 100%);
-`;
-
-const App = () => (
-    <Fragment>
-        <Wrapper>
-            <ApolloProvider client={client}>
-                <ApolloHooksProvider client={client}>
-                    <ThemeProvider theme={theme}>
-                        <BrowserRouter>
-                            <Switch>
-                                <PublicRoute exact path="/" component={Login} />
-                                <PublicRoute
-                                    exact
-                                    path="/signin"
-                                    component={Login}
-                                />
-                                <PublicRoute
-                                    exact
-                                    path="/signup"
-                                    component={Login}
-                                />
-                                <PublicRoute
-                                    exact
-                                    path="/forgot"
-                                    component={Login}
-                                />
-                                <PrivateRoute
-                                    exact
-                                    path="/users"
-                                    component={Users}
-                                />
-                                <PrivateRoute
-                                    exact
-                                    path="/test"
-                                    component={UsersTest}
-                                />
-                            </Switch>
-                        </BrowserRouter>
-                    </ThemeProvider>
-                </ApolloHooksProvider>
-            </ApolloProvider>
-        </Wrapper>
-        <GlobalStyle />
-    </Fragment>
-);
-
-export default App;
+export default App
--- packages/web/src/components/Users/User/User.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const User = () => (
-    <div>
-        <h1>User</h1>
-    </div>
-);
-
-export default User;
