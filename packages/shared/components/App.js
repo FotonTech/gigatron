217c4b9e89102ea43f5d6017d160791ad83fd8f3
Guilherme Jabur <guilhermejabur@outlook.com>
2019-04-13 15:22:26 -0300

    flat shared

--- packages/shared/components/App.js
+++ packages/shared/components/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import Welcome from './Welcome'
-import { View } from 'react-native'
-// import styled from 'styled-components/native'
+import { View, Text } from 'react-native'
+import styled from 'styled-components'
 
-// const Wrapper = styled.View`
-//   flex: 1;
-//   justify-content: center;
-//   align-items: center;
-// `
+const Wrapper = styled(View)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`
+
+const WelcomeText = styled(Text)`
+  color: #a300fb;
+  font-size: 23;
+  margin: 40px;
+`
 
 const App = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Welcome />
-  </View>
+  <Wrapper>
+    <WelcomeText> Welcome to gigatron </WelcomeText>
+  </Wrapper>
 )
 
 export default App
--- packages/shared/components/App.tsx
+++ packages/shared/components/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import Welcome from './Welcome'
-import styled from 'styled-components/native'
+import { View, Text } from 'react-native'
+import styled from 'styled-components'
 
-const Wrapper = styled.View`
+const Wrapper = styled(View)`
   flex: 1;
   justify-content: center;
   align-items: center;
 `
 
-export default () => (
+const WelcomeText = styled(Text)`
+  color: #a300fb;
+  font-size: 23;
+  margin: 40px;
+`
+
+const App = () => (
   <Wrapper>
-    <Welcome />
+    <WelcomeText> Welcome to gigatron </WelcomeText>
   </Wrapper>
 )
+
+export default App
